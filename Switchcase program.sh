f336133f17fc7a868462dcd870ef542d65b87859
Gaya3jp <97285051+Gaya3jp@users.noreply.github.com>
2022-04-28 14:27:10 +0530

    Create Switchcase program.sh

---
+++ Switchcase program.sh
@@ -0,0 +1,44 @@
+ !/bin/bash
+
+echo "Enter the name of the state:"
+read state
+case $state in
+          "Uttar Pradesh")
+                          echo "capital is Lucknow"
+                          echo
+          ;;
+          "Punjab"  | "Haryana")
+                           echo "capital is Chandigarh"
+                           echo 
+         ;;
+          "Bihar")
+                           echo "capital is Patna"
+                           echo
+         ;;
+          "Rajasthan")
+                           echo "capital is Jaipur"
+                                                                                                              1,1           Top
+
+                          echo
+          ;;
+          "Punjab"  | "Haryana")
+                           echo "capital is Chandigarh"
+                           echo 
+         ;;
+          "Bihar")
+                           echo "capital is Patna"
+                           echo
+         ;;
+          "Rajasthan")
+                           echo "capital is Jaipur"
+                           echo
+         ;;
+                   echo
+          ;;
+          *)
+                           echo "You discovered an unknown state"
+                           echo
+         ;;
+esac
+                                                                                                                                                                                         131,1         Bot
+
